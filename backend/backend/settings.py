34fc8a87a43e48fd2166058377bebeb85bd70cf4
Peshkov-Matvei <92847320+Peshkov-Matvei@users.noreply.github.com>
2022-10-09 13:09:29 +1100

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djoser',
     'rest_framework',
     'django_filters',
     'api',
@@ -103,11 +104,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_PAGINATION_CLASS': [
-        'rest_framework.pagination.PageNumberPagination',
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'PAGE_SIZE': 6
 }
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('api/', include('recipes.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
 
 
